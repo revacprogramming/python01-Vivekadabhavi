2023cb94331b6d3bb4655cf51ba412d98783c0b3
Vivek Madabhavi <97020590+Vivekadabhavi@users.noreply.github.com>
2022-07-03 15:26:38 +0000

    3

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,14 @@
 # Variables, Expressions & Statements
 
-hrs = float(input("Enter hours? "))
+hrs = input("Enter hours")
+rate= input("enter rate")
+h=float(hrs)
+r=float(rate)
+if  h >40:
+    a=h*r
+    b=(h-40)*(r*0.5)
+    pay= a+b
+    print(pay)
+else:
+    pay= hrs*rate
+    print(pay)
